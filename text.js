0f75ac99dee8f577758faac5019647e879083129
shambhabya <shambhabyamedhi@gmail.com>
2024-03-20 23:32:12 +0530

    set up project structure and configuration files

---
+++ text.js
@@ -0,0 +1,12 @@
+var arr = [9, 3, 12, 11, 40, 28, 5]
+
+// console.log(arr);
+
+arr.sort((a, b)=>{
+    console.log()
+});
+
+// console.log(arr);
+
+5     11         3    |   9   12       40        28
+35    31-31      33   |   39  31-32    34-30     32-38
